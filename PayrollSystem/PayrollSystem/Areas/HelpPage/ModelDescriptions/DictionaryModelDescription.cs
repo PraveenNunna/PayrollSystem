b111d7cf229d303c3c1b4a6a7ed32f58aa34bf87
Praveen Nunna <PraveenNunna@users.noreply.github.com>
2020-02-12 16:15:39 +0100

    Removed unused files

--- PayrollSystem/PayrollSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PayrollSystem.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- PayrollSystem/PayrollSystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PayrollSystem.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
